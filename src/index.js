9fd1b0e5491b0bb0a54b9696c98ce9cdccd49668
Dang Cao Hau <caohauhau9@gmail.com>
2024-11-27 18:33:53 +0700

    update homepage in file  views/index.ejs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   req.user = { name: "Đặng Cao Hậu", tuoi: 20 };
   res.render("index.ejs", { user: req.user });
